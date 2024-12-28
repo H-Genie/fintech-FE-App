bd67a9602da9482cf943dfe58f4166d08c557936
jinsewoon-dev <jinsewoon.dev@gmail.com>
2024-12-28 23:05:34 +0900

    chore : alias에 assets 추가

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@shadcn": path.resolve(__dirname, "./src/shadcn"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
 });
