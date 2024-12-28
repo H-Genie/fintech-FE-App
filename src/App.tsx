af902d23e55e47dcc80eaf5a814ce52a7df27f2e
jinsewoon-dev <jinsewoon.dev@gmail.com>
2024-12-28 23:02:13 +0900

    feat : @tanstack/react-query 설치 및 provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Outlet } from "react-router-dom";
-
+const queryClient = new QueryClient();
 function App() {
-  return <Outlet />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Outlet />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
