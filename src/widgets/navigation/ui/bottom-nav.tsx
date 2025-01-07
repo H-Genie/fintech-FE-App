b728c22da0d8c692c663c0f80d6102957e1243c1
jinsewoon-dev <jinsewoon.dev@gmail.com>
2025-01-07 21:48:48 +0900

    fix: { cn } import 경로 alias로 수정

--- src/widgets/navigation/ui/bottom-nav.tsx
+++ src/widgets/navigation/ui/bottom-nav.tsx
@@ -1,7 +1,7 @@
 import { useLocation } from 'react-router-dom';
 import { NAVIGATION_ITEMS } from '../model/constants';
 import { NavigationItem } from './nav-item';
-import { cn } from 'src/lib/shadcn/lib/utils';
+import { cn } from '@shared/ui/shadcn/lib/utils';
 
 interface BottomNavigationProps {
   className?: string;
@@ -18,16 +18,16 @@ export const BottomNavigation = ({ className }: BottomNavigationProps) => {
   };
 
   return (
-    <nav 
+    <nav
       className={cn(
-        "fixed bottom-0 left-0 right-0 z-50",
-        "border-t safe-area-bottom",
-        className
+        'fixed bottom-0 left-0 right-0 z-50',
+        'border-t safe-area-bottom',
+        className,
       )}
     >
-      <div className="h-16 max-w-md mx-auto flex items-center justify-around">
+      <div className='h-16 max-w-md mx-auto flex items-center justify-around'>
         {NAVIGATION_ITEMS.map((item) => (
-          <NavigationItem 
+          <NavigationItem
             key={item.id}
             item={item}
             isActive={isActiveRoute(item.path)}
