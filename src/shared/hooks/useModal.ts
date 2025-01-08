5d788fd9b9277517f4c188999d91d63dbd42bafe
jinsewoon-dev <jinsewoon.dev@gmail.com>
2025-01-09 08:59:47 +0900

    feat: modal,dialog render를 위한 openModal,openDialog 매소드 생성

--- src/shared/hooks/useModal.ts
+++
@@ -1,8 +0,0 @@
-import { useModalStore } from '@shared/stores/modal';
-
-//래핑 훅. 추가 로직이 필요할지도..?
-const useModal = () => {
-  return { ...useModalStore() };
-};
-
-export default useModal;
---
+++ src/shared/hooks/useModal.tsx
@@ -0,0 +1,47 @@
+import { useModalStore, type TModalOptions } from '@shared/stores/modal';
+import DialogWrapper, { type TDialog } from '@shared/ui/modal/DialogWrapper';
+import ModalWrapper from '@shared/ui/modal/ModalWrapper';
+
+//래핑 훅. 추가 로직이 필요할지도..?
+const useModal = () => {
+  const {
+    openModal: open,
+    closeAllModal,
+    closeModal,
+    cleanupModals,
+  } = useModalStore();
+
+  const openModal = (children: React.ReactElement, options?: TModalOptions) => {
+    open('modal', <ModalWrapper>{children}</ModalWrapper>, {
+      enableOverlay: options?.enableOverlay ?? true,
+      enableOverlayClickClose: options?.enableOverlayClickClose ?? true,
+      enableBackgroundScroll: options?.enableBackgroundScroll ?? true,
+    });
+  };
+
+  const openDialog = ({
+    title,
+    description,
+    confirm,
+    cancel,
+    confirmButtonText,
+    cancelmButtonText,
+  }: TDialog) => {
+    const config: TDialog = {
+      title,
+      description,
+      confirm,
+      cancel,
+      confirmButtonText,
+      cancelmButtonText,
+    };
+    open('dialog', <DialogWrapper config={config} />, {
+      enableOverlay: true,
+      enableOverlayClickClose: true,
+      enableBackgroundScroll: true,
+    });
+  };
+  return { openModal, openDialog, closeModal, closeAllModal, cleanupModals };
+};
+
+export default useModal;
