18109ede138a0d97f14875ee1e00a07d31967190
h-genie <ses2000hj@hanmail.net>
2025-01-12 21:22:30 +0900

    feat : Pretendard 폰트 설정

--- src/app/layouts/basic-layout.tsx
+++ src/app/layouts/basic-layout.tsx
@@ -2,10 +2,10 @@ import { Outlet } from 'react-router-dom';
 
 export const BasicLayout = () => {
   return (
-    <div className="min-h-screen">
-      <main className="container mx-auto px-4 py-8">
+    <div className='min-h-screen'>
+      <main className='container mx-auto px-4 py-8'>
         <Outlet />
       </main>
     </div>
   );
-}; 
+};
