3c1065f13c3c0544bc28b59ec432799fee4453ce
jinsewoon-dev <jinsewoon.dev@gmail.com>
2025-01-16 20:36:50 +0900

    refactor: Undeveloped컴포넌트 최상위 부모 div를 pageLayout으로 대체

--- src/shared/ui/Undeveloped.tsx
+++ src/shared/ui/Undeveloped.tsx
@@ -1,13 +1,15 @@
+import PageLayout from './PageLayout';
+
 const Undeveloped = () => {
   return (
-    <div className='h-screen flex flex-col items-center justify-center -m-8'>
+    <PageLayout hasNav className='flex flex-col items-center justify-center '>
       <img src='/logo.png' className='w-24' alt='Logo' />
       <h2 className='text-3xl mt-8 mb-8 font-black text-[#18A0FB]'>
         To be continued
       </h2>
 
       <p onClick={() => window.history.back()}>돌아가기</p>
-    </div>
+    </PageLayout>
   );
 };
 
