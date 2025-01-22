4acbf97ae6ff318a993ca61a07f7e1f18bc8a7f9
jinsewoon-dev <jinsewoon.dev@gmail.com>
2025-01-22 18:43:19 +0900

    refacotr: useLocationState state로 변경

--- src/pages/payment/detail/QRPaymentDetailPage.tsx
+++ src/pages/payment/detail/QRPaymentDetailPage.tsx
@@ -1,69 +1,72 @@
-import { ROUTES } from '@constants/routes';
-import { useQueryParams } from '@hooks/useQueryParams';
-import { useOrderInfo } from '@hooks/queries/usePayments';
-import useModal from '@hooks/useModal';
 import PageLayout from '@ui/layouts/PageLayout';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import QRDetailContent from '@ui/templates/qrCode/detail/QRDetailContent';
 import { LoadingSpinner } from '@ui/components/loading/LoadingSpinner';
 import Button from '@ui/components/button/Button';
+import { useLocationState } from '@hooks/useLocationState';
+// import { useEffect } from 'react';
+// import { ROUTES } from '@constants/routes';
+import { useOrderInfo } from '@hooks/queries/usePayments';
 
 const QRPaymentDetailPage = () => {
-  const navigate = useNavigate();
-  const { openDialog, closeModal } = useModal();
-  const { params, isLoading } = useQueryParams();
-  const { token, expiredAt } = params;
+  // const navigate = useNavigate();
+  // const { openDialog, closeModal } = useModal();
 
-  useEffect(() => {
-    if (isLoading) return;
+  const { state } = useLocationState<{
+    token: string;
+    expiredAt: number;
+  }>();
 
-    let errorMessage = '';
+  const { token } = state;
 
-    if (!token) {
-      errorMessage = 'QR 결제 정보가 없습니다.\n다시 시도해 주세요.';
-    } else if (expiredAt && new Date().getTime() > Number(expiredAt)) {
-      errorMessage = '만료된 QR 코드입니다.\n다시 시도해 주세요.';
-    }
-    if (errorMessage) {
-      openDialog('alert', {
-        title: '오류',
-        description: errorMessage,
-        confirm: () => {
-          closeModal();
-          navigate(`${ROUTES.PAYMENT.QR}`);
-        },
-      });
-    }
-  }, [isLoading]);
+  // useEffect(() => {
+  //   if (isLoading) return;
+  //   let errorMessage = '';
+
+  //   if (!token) {
+  //     errorMessage = 'QR 결제 정보가 없습니다.\n다시 시도해 주세요.';
+  //   } else if (expiredAt && new Date().getTime() > Number(expiredAt)) {
+  //     errorMessage = '만료된 QR 코드입니다.\n다시 시도해 주세요.';
+  //   }
+  //   if (errorMessage) {
+  //     openDialog('alert', {
+  //       title: '오류',
+  //       description: errorMessage,
+  //       confirm: () => {
+  //         closeModal();
+  //         navigate(`${ROUTES.PAYMENT.QR}`);
+  //       },
+  //     });
+  //   }
+  // }, [isLoading]);
 
   const {
     data: orderData,
     isLoading: orderLoading,
     isError: orderError,
-    refetch: orderRefech,
-  } = useOrderInfo(token || '');
+    refetch: orderRefetch,
+  } = useOrderInfo(token);
 
   return (
     <PageLayout className='bg-gradient-to-br from-[#3c1488] via-[#408693] to-[#1e7f84] w-full  flex justify-center items-center'>
-      {/* 카드 */}
       <div className='bg-white w-[320px] h-[600px] rounded-[1rem] flex flex-col justify-center items-center p-8'>
-        {orderData && <QRDetailContent orderData={orderData} />}
-        {orderLoading && <LoadingSpinner />}
-        {orderError && (
-          <>
-            <div>에러가 발생했습니다</div>
-            <div>주문 정보를 불러오는데 실패했습니다. </div>
-            <div>다시 시도해 주세요.</div>
-            <Button
-              className='w-[calc(100%-32px)] h-12 text-[#18A0FB] font-bold rounded-full mt-4'
-              isPending={orderLoading}
-              onClick={() => orderRefech()}
-            >
-              재시도 하기
-            </Button>
-          </>
-        )}
+        <>
+          {orderData && <QRDetailContent orderData={orderData} />}
+          {orderLoading && <LoadingSpinner />}
+          {orderError && (
+            <>
+              <div>에러가 발생했습니다</div>
+              <div>주문 정보를 불러오는데 실패했습니다. </div>
+              <div>다시 시도해 주세요.</div>
+              <Button
+                className='w-[calc(100%-32px)] h-12 text-[#18A0FB] font-bold rounded-full mt-4'
+                isPending={orderLoading}
+                onClick={() => orderRefetch()}
+              >
+                재시도 하기
+              </Button>
+            </>
+          )}
+        </>
       </div>
     </PageLayout>
   );
