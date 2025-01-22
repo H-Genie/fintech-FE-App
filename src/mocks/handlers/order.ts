2388169352f8dabe81a20e5fdaf8467468ea0a49
jinsewoon-dev <jinsewoon.dev@gmail.com>
2025-01-22 18:43:37 +0900

    feat: sse mocking api 추가

--- src/mocks/handlers/order.ts
+++ src/mocks/handlers/order.ts
@@ -3,21 +3,44 @@ import { http, HttpResponse } from 'msw';
 
 export const orderHandler = [
   // 주문 정보 조회
-  http.get(
-    `/${API_ENDPOINTS.PAYMENT.ORDER.INFO(':orderToken')}`,
-    ({ params }) => {
-      const { orderToken } = params;
-      return HttpResponse.json({
-        code: 200,
-        data: {
-          orderToken: `order-${orderToken}`,
-          orderId: `order-${orderToken}`,
-          orderName: '테스트 상품 외2',
-          amount: 15000,
-          store: '테스트 가맹점',
-          url: 'https://example.com',
-        },
-      });
-    },
-  ),
+  http.get(API_ENDPOINTS.PAYMENT.ORDER.INFO(':orderToken'), ({ params }) => {
+    const { orderToken } = params;
+    return HttpResponse.json({
+      code: 200,
+      data: {
+        orderToken: `order-${orderToken}`,
+        orderId: `order-${orderToken}`,
+        orderName: '테스트 상품 외2',
+        amount: 15000,
+        store: '테스트 가맹점',
+        url: 'https://example.com',
+      },
+    });
+  }),
+  // sse
+  http.get(API_ENDPOINTS.PAYMENT.ORDER.SSE, () => {
+    const encoder = new TextEncoder();
+    const stream = new ReadableStream({
+      start(controller) {
+        const messages = [
+          'data: {"message": "Event 1"}\n\n',
+          'data: {"message": "Event 2"}\n\n',
+          'data: {"message": "Event 3"}\n\n',
+        ];
+
+        messages.forEach((message, index) => {
+          setTimeout(() => {
+            controller.enqueue(encoder.encode(message));
+            if (index === messages.length - 1) {
+              controller.close();
+            }
+          }, index * 3000); // 3초 간격으로 메시지 전송
+        });
+      },
+    });
+
+    return new HttpResponse(stream, {
+      headers: { 'Content-Type': 'text/event-stream' },
+    });
+  }),
 ];
