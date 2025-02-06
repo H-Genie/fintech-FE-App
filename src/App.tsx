c783da57841088c040a147a36041242e9c555c05
h-genie <ses2000hj@hanmail.net>
2025-02-06 17:47:44 +0900

    fix : 프로덕션 모드에서도 msw 실행

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { router } from './router';
 import ModalProvider from './providers/modalProvider';
 
 //MSW를 개발 환경에서만 시작하도록 설정합니다.
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = await import('@mocks/browser');
-  worker.start();
-}
+// if (process.env.NODE_ENV === 'development') {
+//   const { worker } = await import('@mocks/browser');
+//   worker.start();
+// }
+const { worker } = await import('@mocks/browser');
+worker.start();
 
 const queryClient = new QueryClient();
 function App() {
