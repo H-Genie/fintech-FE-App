cc43826d479241518a7285bf1663426845c8ca40
h-genie <ses2000hj@hanmail.net>
2025-02-07 17:38:13 +0900

    fix : 더미데이터의 토큰을 수정한다

--- src/mocks/config/dummy.ts
+++ src/mocks/config/dummy.ts
@@ -8,3 +8,6 @@ export const DUMMY_API_CONFIG = {
     IAT: 171232323232,
   },
 } as const;
+
+// http://loalhost:5174/payment/deeplink/success?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjaGFudF9uYW1lIjoiZGV2ZWxvcGVyIHNob3AiLCJvcmRlcl9uYW1lIjoibmV4dGpzIDEiLCJhbW91bnQiOjUwMDAwLCJpYXQiOjE1MTYyMzkwMjJ9.JFQ2zCTeF7y5Chq9c-p-vMyQ2wn_RyYX9dTxeI5cSxo=&expiredAt=2025-02-08T08:33:58.879Z
+// https://pay-200.vercel.app/payment/deeplink/success?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjaGFudF9uYW1lIjoiZGV2ZWxvcGVyIHNob3AiLCJvcmRlcl9uYW1lIjoibmV4dGpzIDEiLCJhbW91bnQiOjUwMDAwLCJpYXQiOjE1MTYyMzkwMjJ9.JFQ2zCTeF7y5Chq9c-p-vMyQ2wn_RyYX9dTxeI5cSxo=&expiredAt=2025-02-08T08:33:58.879Z
